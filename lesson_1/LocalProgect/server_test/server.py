b093c5f467e0fae9f881d00dfb4e08d7fda1d220
Andrew <diver.vlz@gmail.com>
2023-01-30 00:51:01 +0300

    almost done

--- lesson_1/LocalProgect/server_test/server.py
+++ lesson_1/LocalProgect/server_test/server.py
@@ -1,60 +1,26 @@
-from wsgiref.simple_server import make_server
+from server_urls import fronts
 
 
-def index_view(request):
-    print(request)
-    return '200 OK', [b'<h1>This page?</h1>']
-
-
-def abc_view(request):
-    return '200 OK', [b'ABC']
-
-
-def home_view(request):
-    return '200 OK', [b'Home view added']
-
-
-def not_found_404_view(request):
-    return '404 WHAT', [b'404 Page Not Found']
-
-
-class Other:
+class PageNotFound404:
     def __call__(self, request):
-        print(request)
-        return '200 OK', [b'<h1>other</h1><h3>text</h3>']
+        return '404 WHAT', '<h1>404 Page Not Found</h1>'
 
-
-routes = {
-    '/': index_view,
-    '/abc/': abc_view,
-    '/home/': home_view,
-    '/other/': Other(),
-}
-
-
-def secret_front(request):
-    request['secret'] = 'some secret'
-
-
-def other_front(request):
-    request['key'] = 'key'
-
-
-fronts = [secret_front, other_front]
 
 class Application:
     def __init__(self, routes, fronts):
         self.routes = routes
         self.fronts = fronts
-        print(self.fronts)
 
     def __call__(self, environ, start_response):
         path = environ['PATH_INFO']
 
+        if not path.endswith('/'):
+            path = f'{path}/'
+
         if path in self.routes:
             view = self.routes[path]
         else:
-            view = not_found_404_view
+            view = PageNotFound404()
         request = {}
 
         for front in fronts:
@@ -63,12 +29,4 @@ class Application:
         code, body = view(request)
 
         start_response(code, [('Content-Type', 'text/html')])
-        return body
-
-
-app = Application(routes, fronts)
-
-
-with make_server('', 8000, app) as httpd:
-    print("Serving on port 8000...")
-    httpd.serve_forever()
+        return [body.encode('utf-8')]
---
+++ lesson_1/LocalProgect/server_test/server_views.py
@@ -0,0 +1,19 @@
+from templator import render
+
+
+class Index:
+    def __call__(self, request):
+        print(request)
+        return '200 OK', render('index.html', date=request.get('secret', None))
+
+
+class HomeView:
+    def __call__(self, request):
+        print(request)
+        return '200 OK', render('home.html', date=request.get('secret', None))
+
+
+class Other:
+    def __call__(self, request):
+        print(request)
+        return '200 OK', render('other.html', date=request.get('key'))
