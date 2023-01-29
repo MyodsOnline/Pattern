4211fd0eb14da710ea3aa3388f42ecb4a2c9d974
Andrew <diver.vlz@gmail.com>
2023-01-29 23:50:46 +0300

    update

--- lesson_1/LocalProgect/server_test/server.py
+++ lesson_1/LocalProgect/server_test/server.py
@@ -1,24 +1,26 @@
 from wsgiref.simple_server import make_server
 
 
-def index_view():
+def index_view(request):
+    print(request)
     return '200 OK', [b'<h1>This page?</h1>']
 
 
-def abc_view():
+def abc_view(request):
     return '200 OK', [b'ABC']
 
 
-def home_view():
+def home_view(request):
     return '200 OK', [b'Home view added']
 
 
-def not_found_404_view():
+def not_found_404_view(request):
     return '404 WHAT', [b'404 Page Not Found']
 
 
 class Other:
-    def __call__(self):
+    def __call__(self, request):
+        print(request)
         return '200 OK', [b'<h1>other</h1><h3>text</h3>']
 
 
@@ -29,10 +31,22 @@ routes = {
     '/other/': Other(),
 }
 
+
+def secret_front(request):
+    request['secret'] = 'some secret'
+
+
+def other_front(request):
+    request['key'] = 'key'
+
+
+fronts = [secret_front, other_front]
+
 class Application:
-    def __init__(self, routes):
-        print(routes)  # {'/': <function index_view>, '/abc/': <function abc_view>, '/home/': <function home_view>}
+    def __init__(self, routes, fronts):
         self.routes = routes
+        self.fronts = fronts
+        print(self.fronts)
 
     def __call__(self, environ, start_response):
         path = environ['PATH_INFO']
@@ -41,16 +55,18 @@ class Application:
             view = self.routes[path]
         else:
             view = not_found_404_view
-        code, body = view()  # <class 'str'> 200 OK, <class 'list'> [b'This page?']
+        request = {}
+
+        for front in fronts:
+            front(request)
+        print(request)
+        code, body = view(request)
+
         start_response(code, [('Content-Type', 'text/html')])
         return body
 
 
-app = Application(routes)
-
-
-
-
+app = Application(routes, fronts)
 
 
 with make_server('', 8000, app) as httpd:
